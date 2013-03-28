c9cf3b295b4115e7309eccb9fff444e86f13c928
Scot Dalton <scotdalton@gmail.com>
2013-03-28 16:44:09 -0400

    Update specs for travis.

--- spec/rake_nyu/puma_config_spec.rb
+++ spec/rake_nyu/puma_config_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 module RakeNyu
   describe PumaConfig, "#initialize" do
+    def dummy_path
+      @dummy_path ||= File.expand_path("../../dummy", __FILE__)
+    end
+
     it "returns correct settings based on config" do
       puma_config = PumaConfig.new('9292')
       puma_config.port.should eq('9292')
       puma_config.bind.should eq('tcp://127.0.0.1:9292')
-      puma_config.pid.should eq('/Users/dalton/Documents/workspace/rake_nyu/spec/dummy/tmp/pids/puma-9292.pid"')
-      puma_config.log.should eq('/Users/dalton/Documents/workspace/rake_nyu/spec/dummy/log/puma-test-9292.log')
-      puma_config.scripts_path.should eq('/Users/dalton/Documents/workspace/rake_nyu/spec/dummy/config/deploy/puma')
+      puma_config.pid.should eq("#{dummy_path}/tmp/pids/puma-9292.pid")
+      puma_config.log.should eq("#{dummy_path}/log/puma-test-9292.log")
+      puma_config.scripts_path.should eq("#{dummy_path}/config/deploy/puma")
     end
   end
 end
