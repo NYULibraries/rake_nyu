7cf4dcf8071555279a78b568b748f892397c2b98
hab278 <as>
2013-07-11 13:55:39 -0400

    Had a problem with rvm, was setting rvm_type to wsops isntead of user

--- lib/capistrano-nyu/rvm.rb
+++ lib/capistrano-nyu/rvm.rb
@@ -5,17 +5,12 @@ require "rvm/capistrano"
 Capistrano::Configuration.instance(:must_exist).load do
   # Make sure correct ruby is installed
   before  "deploy",         "rvm:install_ruby"
-  before  "deploy",         "rvm:get_user"
   # After your bundle has installed, do any migrations
   after   "bundle:install", "deploy:migrate"
   
   # RVM  vars
   set :rvm_ruby_string, fetch(:rvm_ruby_string, "ruby-1.9-p448")
-  namespace :rvm do
-    task :get_user do
-      set :rvm_type, fetch(:user)
-    end
-  end
+  set :rvm_type, :user
   
   # Bundler vars
   set :bundle_without, [:development, :test]
