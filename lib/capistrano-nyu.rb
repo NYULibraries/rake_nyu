1807662634fe31a2ab92a5868caebc5d5afb9d51
hab278 <as>
2013-07-12 15:05:47 -0400

    Fixes #3

--- lib/capistrano-nyu.rb
+++ lib/capistrano-nyu.rb
@@ -1,4 +1,6 @@
 Capistrano::Configuration.instance(:must_exist).load do
+  # Hack on Capistrano, Capistrnao requires :repository to be set, and thus is always set with garbage value.
+  # This makes it impossible to detect wether or not the user set it, so unsetting here solves that.
   unset :repository
   require_relative 'capistrano-nyu/recipes'
 end
