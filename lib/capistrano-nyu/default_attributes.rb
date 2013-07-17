fc712ed0f2d6597a9c192a1e4cdf00e78bc6cb23
hab278 <as>
2013-07-17 13:28:50 -0400

    Git tagging working a little bit better.

--- lib/capistrano-nyu/default_attributes.rb
+++ lib/capistrano-nyu/default_attributes.rb
@@ -1,6 +1,21 @@
 # Including Capistrano Tagging recipes
 require 'capistrano/tagging'
 Capistrano::Configuration.instance(:must_exist).load do
+  before 'tagging:deploy', 'tagging:checkout_branch'
+  after 'tagging:deploy', 'tagging:return_to_deploy'
+  namespace :tagging do
+    task :checkout_branch do
+      run "cd #{current_release} && git checkout #{revision}; true" do |channel, stream, data|
+        if stream.eql? :err
+          run "cd #{current_release} && git checkout -b #{revision}; true"
+        end
+      end
+    end
+    
+    task :return_to_deploy do
+      run "cd #{current_release} && git checkout deploy; true"
+    end
+  end
   # SSH options
   set :ssh_options, {:forward_agent => true}
   
