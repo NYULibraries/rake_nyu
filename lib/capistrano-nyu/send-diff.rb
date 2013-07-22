442d890b0c10641a3e77b61de9fe40c6644ed3e7
hab278 <as>
2013-07-22 15:34:41 -0400

    Forgot to return the valie

--- lib/capistrano-nyu/send-diff.rb
+++ lib/capistrano-nyu/send-diff.rb
@@ -18,7 +18,7 @@ Capistrano::Configuration.instance(:must_exist).load do
         git_link = "https://www.github.com/#{repo_name}/compare/#{from.commit.sha}...#{to.commit.sha}"
         run "curl -i http://git.io -F \"url=#{git_link}\"" do |channel, stream, data|
           if data.include? "Location:"
-            data.scan(/Location:\s+(.*)/).last.first
+            return data.scan(/Location:\s+(.*)/).last.first
           end
         end
       end
