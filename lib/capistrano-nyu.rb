eacc255c6d90afbe535b819966a5f7be0c9069db
hab278 <as>
2013-07-23 12:19:00 -0400

    Unsetting :application as well

--- lib/capistrano-nyu.rb
+++ lib/capistrano-nyu.rb
@@ -1,7 +1,9 @@
 Capistrano::Configuration.instance(:must_exist).load do
   # Hack on Capistrano, Capistrnao requires :repository to be set, and thus is always set with garbage value.
   # This makes it impossible to detect wether or not the user set it, so unsetting here solves that.
+  # Also, we set it in capistrano-nyu/default_attributes anyway, so it's safe as long as that file is required.
   unset :repository
+  unset :application
   
   # Requiring the default recipe list, will make most apps have a two line long deploy.rb file.
   require_relative 'capistrano-nyu/default_recipes'
