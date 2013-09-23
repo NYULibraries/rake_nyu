7d502a41cde936f47784e441b0adeb5e728378ea
hab278 <hab278@nyu.edu>
2013-09-23 18:28:51 -0400

    fix #12

--- lib/nyulibraries/deploy.rb
+++ lib/nyulibraries/deploy.rb
@@ -1,5 +1,8 @@
 module NyuLibraries
   module Deploy
+    require_relative 'deploy/newrelic_manager'
+    require_relative 'deploy/puma_config'
+    require_relative 'deploy/puma_manager'
     require_relative 'deploy/rake'
   end
 end
