c943d5a33b7cab30c35b4b3069180c1c6b948e34
hab278 <hab278@nyu.edu>
2013-09-23 16:41:32 -0400

    Updating the tests too

--- spec/rake_nyu/puma_config_spec.rb
+++ spec/rake_nyu/puma_config_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 module NyuLibraries
-  describe PumaConfig, "#initialize" do
-    def dummy_path
-      @dummy_path ||= File.expand_path("../../dummy", __FILE__)
-    end
-
-    it "returns correct settings based on config" do
-      puma_config = PumaConfig.new('9292')
-      puma_config.port.should eq('9292')
-      puma_config.bind.should eq('tcp://127.0.0.1:9292')
-      puma_config.pid.should eq("#{dummy_path}/tmp/pids/puma-9292.pid")
-      puma_config.log.should eq("#{dummy_path}/log/puma-test-9292.log")
-      puma_config.scripts_path.should eq("#{dummy_path}/config/deploy/puma")
+  module Deploy
+    describe PumaConfig, "#initialize" do
+      def dummy_path
+        @dummy_path ||= File.expand_path("../../dummy", __FILE__)
+      end
+    
+      it "returns correct settings based on config" do
+        puma_config = PumaConfig.new('9292')
+        puma_config.port.should eq('9292')
+        puma_config.bind.should eq('tcp://127.0.0.1:9292')
+        puma_config.pid.should eq("#{dummy_path}/tmp/pids/puma-9292.pid")
+        puma_config.log.should eq("#{dummy_path}/log/puma-test-9292.log")
+        puma_config.scripts_path.should eq("#{dummy_path}/config/deploy/puma")
+      end
     end
   end
 end
