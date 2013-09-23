958f5977ff58007c6fa19bfd7d247961e2bf55da
hab278 <hab278@nyu.edu>
2013-09-23 18:26:26 -0400

    This little method fixes #25

--- lib/nyulibraries/deploy/capistrano/assets.rb
+++ lib/nyulibraries/deploy/capistrano/assets.rb
@@ -5,6 +5,11 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   namespace :deploy do
     namespace :assets do
+      def changed_gem_assets gem_name
+        gem_revision = capture("cd #{fetch :latest_release} && awk '/#{gem_name}/{getline; print}' Gemfile.lock").to_s.strip!
+        capture("cd #{fetch :latest_release} && git diff Gemfile.lock | grep '#{gem_revision}' | wc -l").to_i
+      end
+      
       desc "Precompiles if assets have been changed"
       task :precompile, :roles => :web, :except => { :no_release => true } do
         force_compile = false
@@ -18,6 +23,9 @@ Capistrano::Configuration.instance(:must_exist).load do
         rescue Exception => e
           logger.info "Error: #{e}, forcing precompile"
           force_compile = true
+        end
+        fetch(:assets_gem, []).each do |gem_name|
+          changed_asset_count += changed_gem_assets gem_name
         end
         if changed_asset_count > 0 || force_compile
           logger.info "#{changed_asset_count} assets have changed. Pre-compiling"
