b8c1614285a49dd1089923b6052ec77be63c0fc7
hab278 <hab278@nyu.edu>
2013-10-15 12:45:57 -0400

    cleaning up bundler

--- lib/nyulibraries/deploy/capistrano/bundler.rb
+++ lib/nyulibraries/deploy/capistrano/bundler.rb
@@ -4,4 +4,12 @@ require "bundler/capistrano"
 Capistrano::Configuration.instance(:must_exist).load do
   # After your bundle has installed, do any migrations
   after "bundle:install", "deploy:migrate"
+  after "bundle:install", "bundle:clean"
+  
+  namespace :bundle do
+    desc "Clear rails cache"
+    task :clean do
+     run "cd #{current_release} && bundle clean"
+    end
+  end
 end
