cf90f5462c67b2caaa44814fcf3d01b7c36a9bde
hab278 <hab278@nyu.edu>
2013-12-16 15:58:18 -0500

    Split capistrano tasks into cap files

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ source "http://rubygems.org"
 gemspec
 
 gem "coveralls", "~> 0.7.0", require: false, group: :test
-gem "sshkit", :git => "git@github.com:kirs/sshkit.git", :branch => "command-map"
+# gem "sshkit", :git => "git@github.com:kirs/sshkit.git", :branch => "command-map"
 gem "ruby-debug"
 # gem "capistrano", :git => "git://github.com/capistrano/capistrano.git"
 # gem "capistrano-bundler", :git => "git://github.com/capistrano/bundler.git"
--- lib/capistrano/nyulibraries_deploy.rb
+++ lib/capistrano/nyulibraries_deploy.rb
@@ -1,11 +1,10 @@
 require 'capistrano'
 require 'capistrano/bundler'
-
-# require 'pry';pry
-# 
 require 'capistrano/rvm'
+
 load File.expand_path('../tasks/defaults.cap', __FILE__)
 load File.expand_path('../tasks/server_config.cap', __FILE__)
 load File.expand_path('../tasks/tagging.cap', __FILE__)
 load File.expand_path('../tasks/bundler.cap', __FILE__)
 load File.expand_path('../tasks/rvm.cap', __FILE__)
+load File.expand_path('../tasks/rpm.cap', __FILE__)
