996e67097d7e1722ad7e4e3615b85502d29ebf73
hab278 <hab278@nyu.edu>
2014-02-07 09:17:59 -0500

    Dont use rails runner

--- lib/nyulibraries/deploy/capistrano/rails_config.rb
+++ lib/nyulibraries/deploy/capistrano/rails_config.rb
@@ -12,18 +12,18 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :set_variables do
       # Configure app_settings from rails_config
       # Defer processing until we have rails environment
-      set(:app_settings)  { eval(run_locally("rails runner -e #{fetch(:rails_env, 'staging')} 'p Settings.capistrano.to_hash'")) }
-      set(:scm_username)  { app_settings[:scm_username] }
-      set(:app_path)      { app_settings[:path] }
-      set(:user)          { app_settings[:user] }
-      set(:puma_ports)    { app_settings[:puma_ports] }
-      set(:deploy_to)     {"#{fetch :app_path}#{fetch :application}"}
+      # set(:app_settings)  { eval(run_locally("rails runner -e #{fetch(:rails_env, 'staging')} 'p Settings.capistrano.to_hash'")) }
+      # set(:scm_username)  { app_settings[:scm_username] }
+      # set(:app_path)      { app_settings[:path] }
+      # set(:user)          { app_settings[:user] }
+      # set(:puma_ports)    { app_settings[:puma_ports] }
+      # set(:deploy_to)     {"#{fetch :app_path}#{fetch :application}"}
     end
 
     # desc "Set RailsConfig servers"
     task :set_servers do
-      server "#{app_settings[:servers].first}", :app, :web, :db, :primary => true
-      app_settings[:servers].slice(1, app_settings[:servers].length-1).each do |host|
+      server "#{fetch(:servers).first}", :app, :web, :db, :primary => true
+      fetch(:servers).slice(1,fetch(:servers).length-1).each do |host|
         server "#{host}", :app, :web
       end
     end
@@ -37,7 +37,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       else
         p "Variables already set."
       end
-      p "Settings: #{fetch :app_settings}"
+      # p "Settings: #{fetch :app_settings}"
       p "Servers: #{find_servers}"
       p "SCM Username: #{fetch :scm_username}"
       p "App Path: #{fetch :app_path}"
