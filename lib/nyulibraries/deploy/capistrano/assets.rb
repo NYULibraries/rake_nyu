5d47027db56ada6cfadf342aaba7bb1631873183
hab278 <hab278@nyu.edu>
2014-04-15 17:21:21 -0400

    Moving old manifest first

--- lib/nyulibraries/deploy/capistrano/assets.rb
+++ lib/nyulibraries/deploy/capistrano/assets.rb
@@ -2,14 +2,14 @@ require 'bundler'
 
 Capistrano::Configuration.instance(:must_exist).load do
   before 'deploy:finalize_update', 'deploy:assets:symlink'
-  
+
   namespace :deploy do
     namespace :assets do
       def changed_gem_assets gem_name
         gem_revision = capture("cd #{fetch :latest_release} && awk '/#{gem_name}/{getline; print}' Gemfile.lock").to_s.strip!
         capture("cd #{fetch :latest_release} && git diff #{fetch :previous_revision} | grep '#{gem_revision}' | wc -l").to_i
       end
-      
+
       desc "Precompiles if assets have been changed"
       task :precompile, :roles => :web, :max_hosts => 1, :except => { :no_release => true } do
         force_compile = fetch(:force_precompile, false)
@@ -33,6 +33,7 @@ Capistrano::Configuration.instance(:must_exist).load do
           run_locally ("bundle exec rake assets:clean")
           run_locally ("bundle exec rake assets:precompile")
           run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+          run "cd #{shared_path} && mv assets/manifest* #{previous_release}/assets_manifest.json"
           top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
           run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
           run_locally "rm public/assets.tar.bz2"
